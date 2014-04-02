c0b3ca6253f3036432180e98e575141ade3cc712
Justin Henricks <justinhenricks@gmail.com>
2014-04-02 16:28:26 -0400

    Initial interface build by Tim and Z

--- UBRApp/src/edu/hartford/ubr1app/MainActivity.java
+++ UBRApp/src/edu/hartford/ubr1app/MainActivity.java
@@ -6,16 +6,43 @@ import com.example.myfirstapp.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 //test comment
 public class MainActivity extends Activity {
 
+	private boolean connected;
+	
+	private Button connectButton;
+	private Button driveButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        connected = false;
+        
+        this.updateViews();
+        
+        connectButton.setOnClickListener(new View.OnClickListener() {
 
+	        public void onClick(View view) {
+	        	//Intent connectIntent = new Intent(this, going to);
+	        	//startActivity(connectIntent);
+	        }
+	    });
+        
+        driveButton.setOnClickListener(new View.OnClickListener() {
+
+	        public void onClick(View view) {
+	        	//Intent driveIntent = new Intent(this, going to);
+	        	//startActivity(driveIntent);
+	        }
+	    });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,4 +51,20 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    /**
+     * Call updateViews when something changes in the application
+     */
+    private void updateViews(){
+    	//update connect button
+    	Button connect = (Button)findViewById(R.id.connect); 
+    	Button drive = (Button)findViewById(R.id.drive);
+    	if(connected){
+    		connect.setText(R.string.connect);
+    		drive.setVisibility(View.VISIBLE);
+    	}
+    	else{
+    		connect.setText(R.string.disconnect);
+    		drive.setVisibility(View.INVISIBLE);
+    	}
+    }
 }
