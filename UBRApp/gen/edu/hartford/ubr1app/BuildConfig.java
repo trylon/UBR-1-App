f32cb82bfc3914d2f0e763e99896f9295af1d7e7
Justin Henricks <justinhenricks@gmail.com>
2014-04-08 17:18:10 -0400

    fixed android manifest.xml package name

--- UBRApp/gen/edu/hartford/ubr1app/BuildConfig.java
+++ UBRApp/gen/edu/hartford/ubr1app/BuildConfig.java
@@ -1,5 +1,7 @@
+/*___Generated_by_IDEA___*/
+
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.myfirstapp;
+package edu.hartford.ubr1app;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
---
+++ UBRApp/gen/edu/hartford/ubr1app/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package edu.hartford.ubr1app;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
