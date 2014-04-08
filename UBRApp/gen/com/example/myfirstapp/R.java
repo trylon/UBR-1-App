f83678ca9d7bc942b45c6af0fcc1b64882b36e52
bShipman <brShipman@hartford.edu>
2014-04-08 18:10:38 -0400

    Added 6 control buttons and video view.

--- UBRApp/gen/com/example/myfirstapp/R.java
+++ UBRApp/gen/com/example/myfirstapp/R.java
@@ -21,10 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_arrowup=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Button01=0x7f080001;
+        public static final int Button02=0x7f080003;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080002;
+        public static final int button2=0x7f080005;
+        public static final int button3=0x7f080004;
+        public static final int button4=0x7f080006;
+        public static final int videoView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,10 +41,15 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Forward=0x7f050003;
+        public static final int Stop=0x7f050008;
+        public static final int StrafeLeft=0x7f050004;
+        public static final int StrafeRight=0x7f050005;
+        public static final int TurnLeft=0x7f050007;
+        public static final int TurnRight=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int black=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int black=0x7f050002;
     }
     public static final class style {
         /** 
