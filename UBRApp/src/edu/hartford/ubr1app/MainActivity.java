bb1bdaba7018dbb3be20f65dbc38da7cd0dfa88c
Robert Hilton <robert.a.hilton.jr@gmail.com>
2014-04-10 18:22:50 -0400

    Integrated all activities and repaired methods.

--- UBRApp/src/edu/hartford/ubr1app/MainActivity.java
+++ UBRApp/src/edu/hartford/ubr1app/MainActivity.java
@@ -21,67 +21,66 @@ public class MainActivity extends Activity {
 	private Button connectButton;
 	private Button driveButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        connected = false;
-        updateViews();
-        
-        driveButton.setOnClickListener(new View.OnClickListener() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		connected = false;
+		updateViews();
 
-	        public void onClick(View view) {
-	        	/*Intent driveIntent = new Intent(this, DashboardActivity.class);
-	        	driveIntent.putExtra(IP_PORT, socketString);
-	        	startActivity(driveIntent);*/
-	        }
-	    });
-    }
+	}
 
-    public void connectOnClick(View view)
-    {
-    	Intent connectIntent = new Intent(this, ConnectionActivity.class);
-        startActivity(connectIntent); 
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    @Override
-    public void onResume(){
-    	super.onResume();
-    	
-    	/*Intent returnIntent = getIntent();
-    
-    	socketString = returnIntent.getStringExtra(ConnectionActivity.IP_PORT);
-    	//Have Connection activity send us this specific test ip for now...
-    	if(socketString.equals("127.0.0.1:4096"))
-    	{
-    		connected = true;
-    		updateViews();
-    	}*/
-    	
-    	
-    }
-    
-    /**
-     * Call updateViews when something changes in the application
-     */
-    private void updateViews(){
-    	//update connect button
-    	connectButton = (Button)findViewById(R.id.connect); 
-    	driveButton = (Button)findViewById(R.id.drive);
-    	if(connected){
-    		connectButton.setText(R.string.disconnect);
-    		driveButton.setVisibility(View.VISIBLE);
-    	}
-    	else{
-    		connectButton.setText(R.string.connect);
-    		driveButton.setVisibility(View.INVISIBLE);
-    	}
-    }
+	public void connectOnClick(View view) {
+		Intent connectIntent = new Intent(this, ConnectionActivity.class);
+		startActivity(connectIntent);
+	}
+
+	public void driveOnClick(View view) {
+		Intent driveIntent = new Intent(this, DashboardActivity.class);
+		driveIntent.putExtra(IP_PORT, socketString);
+		startActivity(driveIntent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		Intent returnIntent = getIntent();
+		if (returnIntent != null) {
+			socketString = returnIntent
+					.getStringExtra(ConnectionActivity.IP_PORT);
+			if (socketString != null) {
+				// Have Connection activity send us this specific test ip for
+				// now...
+				if (socketString.equals("127.0.0.1:4096")) {
+					connected = true;
+					updateViews();
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Call updateViews when something changes in the application
+	 */
+	private void updateViews() {
+		// update connect button
+		connectButton = (Button) findViewById(R.id.connect);
+		driveButton = (Button) findViewById(R.id.drive);
+		if (connected) {
+			connectButton.setText(R.string.disconnect);
+			driveButton.setVisibility(View.VISIBLE);
+		} else {
+			connectButton.setText(R.string.connect);
+			driveButton.setVisibility(View.INVISIBLE);
+		}
+	}
 }
