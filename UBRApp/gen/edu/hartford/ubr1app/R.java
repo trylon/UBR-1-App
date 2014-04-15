d60900561278502d811c35fd6cec9b16ab7abaaa
htiml <hartfordart.photo@gmail.com>
2014-04-15 06:40:05 -0400

    Added drawable: Nao header image for Main Interface Activity

--- UBRApp/gen/edu/hartford/ubr1app/R.java
+++ UBRApp/gen/edu/hartford/ubr1app/R.java
@@ -165,8 +165,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int nao_launch=0x7f020001;
-        public static final int nao_splash=0x7f020002;
+        public static final int nao_head=0x7f020001;
+        public static final int nao_launch=0x7f020002;
+        public static final int nao_splash=0x7f020003;
     }
     public static final class id {
         public static final int Button01=0x7f090006;
