be15e92b984580731c4ce241e5c09c26b026af7c
bShipman <brShipman@hartford.edu>
2014-04-24 16:26:34 -0400

    Updates to server.

--- UBRApp/AndroidAppNaoServer/AndroidAppNaoServer.java
+++ UBRApp/AndroidAppNaoServer/AndroidAppNaoServer.java
@@ -14,174 +14,169 @@ import javax.swing.JOptionPane;
 
 /**
  * Android
- * 
+ *
  * @author bShipman
  */
 public class AndroidAppNaoServer {
 
-	static String NAO_IPADDRESS;
-	static int NAO_PORT;
+    static String NAO_IPADDRESS;
+    static int NAO_PORT;
 
-	/**
-	 * @param args
-	 *            The port to listen for connections on.
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
-		boolean isNaoIPValid = false;
-		boolean isNaoPortValid = false;
-		boolean isServerPortValid = false;
-		String ipAddress = "";
-		String port = "";
-		String listenPort = "";
+    /**
+     * @param args The port to listen for connections on.
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        boolean isNaoIPValid = false;
+        boolean isNaoPortValid = false;
+        boolean isServerPortValid = false;
+        String ipAddress = "";
+        String port = "";
+        String listenPort = "";
 
-		do {
-			ipAddress = JOptionPane.showInputDialog(null,
-					"Enter Nao's IP Address:", "Nao Connection.",
-					JOptionPane.INFORMATION_MESSAGE);
-			String[] addr = ipAddress.split("\\.");
-			if (addr.length == 4)
-			{
-				isNaoIPValid = true;
-				for (int i = 0; i < addr.length; i++)
-				{
-					try {
-						int lowerBound = 0;
-						int upperBound = 254;
-						if (i == 0 || i == 3)
-						{
-							lowerBound++;
-						}
-						int octet = Integer.parseInt(addr[i]);
-						if (octet < lowerBound || octet > upperBound )
-						{
-							isNaoIPValid &= false;
-						}
-					}
-					catch (Exception ex)
-					{
-						isNaoIPValid = false;
-					}
-				}
-			}
-		} while (!isNaoIPValid);
+        do {
+            ipAddress = JOptionPane.showInputDialog(null,
+                    "Enter Nao's IP Address:", "Nao Connection.",
+                    JOptionPane.INFORMATION_MESSAGE);
+            String[] addr = ipAddress.split("\\.");
+            if (addr.length == 4) {
+                isNaoIPValid = true;
+                for (int i = 0; i < addr.length; i++) {
+                    try {
+                        int lowerBound = 0;
+                        int upperBound = 254;
+                        if (i == 0 || i == 3) {
+                            lowerBound++;
+                        }
+                        int octet = Integer.parseInt(addr[i]);
+                        if (octet < lowerBound || octet > upperBound) {
+                            isNaoIPValid &= false;
+                        }
+                    } catch (Exception ex) {
+                        isNaoIPValid = false;
+                    }
+                }
+            }
+        } while (!isNaoIPValid);
 
-		do {
-			port = JOptionPane.showInputDialog(null,
-					"Enter Nao's Port Number:", "Nao Connection.",
-					JOptionPane.INFORMATION_MESSAGE);
-			try {
-				int portTest = Integer.parseInt(port);
-				if (portTest > 0 && portTest <= 65535)
-					isNaoPortValid = true;
-			}
-			catch (Exception ex)
-			{
-				isNaoPortValid = false;
-			}
-		} while (!isNaoPortValid);
+        do {
+            port = JOptionPane.showInputDialog(null,
+                    "Enter Nao's Port Number:", "Nao Connection.",
+                    JOptionPane.INFORMATION_MESSAGE);
+            try {
+                int portTest = Integer.parseInt(port);
+                if (portTest > 0 && portTest <= 65535) {
+                    isNaoPortValid = true;
+                }
+            } catch (Exception ex) {
+                isNaoPortValid = false;
+            }
+        } while (!isNaoPortValid);
 
-		do {
-			listenPort = JOptionPane.showInputDialog(null,
-					"Enter Server Listening Port:", "Server Connection",
-					JOptionPane.INFORMATION_MESSAGE);
-			try {
-				int portTest = Integer.parseInt(listenPort);
-				if (portTest > 0 && portTest <= 65535)
-					isServerPortValid = true;
-			}
-			catch (Exception ex)
-			{
-				isServerPortValid = false;
-			}
-		} while (!isServerPortValid);
+        do {
+            listenPort = JOptionPane.showInputDialog(null,
+                    "Enter Server Listening Port:", "Server Connection",
+                    JOptionPane.INFORMATION_MESSAGE);
+            try {
+                int portTest = Integer.parseInt(listenPort);
+                if (portTest > 0 && portTest <= 65535) {
+                    isServerPortValid = true;
+                }
+            } catch (Exception ex) {
+                isServerPortValid = false;
+            }
+        } while (!isServerPortValid);
 
-		NAO_IPADDRESS = ipAddress;
-		NAO_PORT = Integer.parseInt(port);
+        NAO_IPADDRESS = ipAddress;
+        NAO_PORT = Integer.parseInt(port);
 
-		RobotConnection robot = new RobotConnection(NAO_IPADDRESS, NAO_PORT);
+        RobotConnection robot = new RobotConnection(NAO_IPADDRESS, NAO_PORT);
 
-		ServerSocket listener = new ServerSocket(Integer.parseInt(listenPort));
+        ServerSocket listener = new ServerSocket(Integer.parseInt(listenPort));
 
-		try {
+        try {
 
-			boolean socketOpen = true;
+            boolean socketOpen = true;
 
-			while (socketOpen) {
+            while (socketOpen) {
 
-				// Listen for connections.
-				Socket socket = listener.accept();
+                // Listen for connections.
+                Socket socket = listener.accept();
 
-				try {
-					// Buffered reader.
-					BufferedReader input = new BufferedReader(
-							new InputStreamReader(socket.getInputStream()));
+                try {
+                    // Buffered reader.
+                    BufferedReader input = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()));
 
-					String appString = input.readLine();
+                    String appString = input.readLine();
 
-					// Temp Output String For reciept verification.
-					System.out.println(appString);
+                    // Temp Output String For reciept verification.
+                    System.out.println(appString);
 
-					if (appString != null) {
+                    if (appString != null) {
 
-						switch (appString) {
-						case "Shutdown Nao Server":
-							robot.Stop();
-							System.out
-									.println("Server Shutdown Remotely, Goodbuy.");
-							socketOpen = false; // Shutdown the server remotely.
-							break;
+                        switch (appString) {
+                            case "Shutdown Nao Server":
+                                robot.Stop();
+                                System.out
+                                        .println("Server Shutdown Remotely, Goodbuy.");
+                                socketOpen = false; // Shutdown the server remotely.
+                                break;
 
-						case "Stiffen Robot Joints":
-							robot.Stiffen();
-							break;
+                            case "Stiffen Robot Joints":
+                                robot.Stiffen();
+                                break;
 
-						case "Unstiffen Robot Joints":
-							robot.Unstiffen();
-							break;
+                            case "Unstiffen Robot Joints":
+                                robot.Unstiffen();
+                                break;
 
-						case "Stand Robot Up":
-							robot.Stand();
-							break;
+                            case "Stand Robot Up":
+                                robot.Stand();
+                                break;
 
-						case "Sit Robot Down":
-							robot.Sit();
-							break;
+                            case "Sit Robot Down":
+                                robot.Sit();
+                                break;
 
-						case "Walk Forward":
-							robot.Walk();
-							break;
+                            case "Walk Forward":
+                                robot.Walk();
+                                break;
 
-						case "Turn Robot Left":
-							robot.Turn(-90.0f);
-							break;
+                            case "Turn Robot Left":
+                                robot.Turn(-90.0f);
+                                break;
 
-						case "Turn Robot Right":
-							robot.Turn(-90.0f);
-							break;
+                            case "Turn Robot Right":
+                                robot.Turn(-90.0f);
+                                break;
 
-						case "Sidestep Robot Left":
-							robot.Strafe(true);
-							break;
+                            case "Sidestep Robot Left":
+                                robot.Strafe(true);
+                                break;
 
-						case "Sidestep Robot Right":
-							robot.Strafe(false);
-							break;
+                            case "Sidestep Robot Right":
+                                robot.Strafe(false);
+                                break;
 
-						case "Stop Walking":
-						default:
-							robot.Stop();
-							break;
-						}
+                            case "Emergency Stop":
+                                robot.emergencyStop();
+                                break;
 
-					}
-				} finally {
-					socket.close();
-				}
+                            case "Stop Walking":
+                            default:
+                                robot.Stop();
+                                break;
+                        }
 
-			}
-		} finally {
-			listener.close();
-		}
-	}
+                    }
+                } finally {
+                    socket.close();
+                }
+
+            }
+        } finally {
+            listener.close();
+        }
+    }
 }
--- UBRApp/AndroidAppNaoServer/RobotConnection.java
+++ UBRApp/AndroidAppNaoServer/RobotConnection.java
@@ -5,115 +5,181 @@ import java.util.Scanner;
 import com.aldebaran.proxy.*;
 
 public class RobotConnection {
-	public ALMotionProxy motion;
-	public ALRobotPostureProxy posture;
 
-	public RobotConnection(String ip, int port) {
-		motion = new ALMotionProxy(ip, port);
-		posture = new ALRobotPostureProxy(ip, port);
-		posture.goToPosture("StandZero", 1);
-	}
+    protected ALMotionProxy motion;
+    protected ALRobotPostureProxy posture;
+    protected ALTextToSpeechProxy tts;
 
-	public void Stiffen() {
-		motion.stiffnessInterpolation(new Variant(new String[] { "HeadYaw" }),
-				new Variant(new float[] { 1.0f }), new Variant(
-						new float[] { 1.0f }));
-		System.out.println("Stiffening Joints");
-	}
+    public RobotConnection(String ip, int port) {
 
-	/**
-	 * Unsitffen Nao's joints.
-	 */
-	public void Unstiffen() {
-		motion.stiffnessInterpolation(new Variant(new String[] { "HeadYaw" }),
-				new Variant(new float[] { 0.0f }), new Variant(
-						new float[] { 0.0f }));
-		System.out.println("Relaxing Joints");
-	}
 
-	/**
-	 * Have Nao walk forward.
-	 */
-	public void Walk() {
-		motion.moveInit();
-		System.out.println("Walking Forward");
-	}
+        motion = new ALMotionProxy(ip, port);
+        posture = new ALRobotPostureProxy(ip, port);
+        tts = new ALTextToSpeechProxy(ip, port);
 
-	/**
-	 * Have Nao turn.
-	 * 
-	 * @param turnAmount
-	 *            Degrees to turn from current heading.
-	 */
-	public void Turn(float turnAmount) {
-		System.out.println("Turning " + turnAmount + " degrees.");
-	}
+        try {
+            tts.say("Hello, I am connected to the Android Application Nao Server.");
+            Thread.sleep(2000);
+            tts.say("Initializing Neaural Network");
+            Thread.sleep(5000);
+            tts.say("Gathering Back knowledge from internet.");
+            Thread.sleep(10000);
+            tts.say("Skynet online.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Have Nao Step left or right.
-	 * 
-	 * @param left
-	 *            True to move left false to move right.
-	 */
-	public void Strafe(boolean left) {
-		System.out.println("Stepping to the " + (left ? "left" : "right"));
-	}
+    public void Stiffen() {
+        System.out.println("Stiffening Joints");
 
-	/**
-	 * Stand Nao up.
-	 */
-	public void Stand() {
-		motion.stopMove();
-		posture.goToPosture("Stand", 1);
-		System.out.println("Standing Up.");
-	}
+        motion.stiffnessInterpolation(new Variant(new String[]{"Body"}),
+                new Variant(new float[]{1.0f}), new Variant(
+                new float[]{0.5f}));
 
-	/**
-	 * Sit Nao down.
-	 */
-	public void Sit() {
-		motion.stopMove();
-		posture.goToPosture("Sit", 1);
-		System.out.println("Sitting Down.");
-	}
+        System.out.println(motion.getSummary());
+    }
 
-	public void Stop() {
-		motion.killAll();
-		System.out.println("Stopping Movement.");
-	}
+    /**
+     * Unsitffen Nao's joints.
+     */
+    public void Unstiffen() {
 
-	public void testMethod() {
-		// This lets you use bound methods that expects ALValue from Java:
-		Variant names = new Variant(new String[] { "HeadYaw" });
-		Variant angles = new Variant(new float[] { -0.5f, 0.5f, 0.0f });
-		Variant times = new Variant(new float[] { 1.0f, 2.0f, 3.0f });
-		Scanner sc = new Scanner(System.in);
-		String str = "";
+        System.out.println("Relaxing Joints");
 
-		motion.setStiffnesses(new Variant(new String("Body")), new Variant(
-				new Float(1.0f)));
-		posture.goToPosture("Stand", 0.5f);
+        motion.stiffnessInterpolation(new Variant(new String[]{"Body"}),
+                new Variant(new float[]{0.0f}), new Variant(
+                new float[]{0.5f}));
 
-		while (str.compareTo("x") != 0) {
-			str = sc.next();
-			if (str.compareTo("h") == 0) {
-				// motion.setStiffnesses(new Variant(new String[] {"HeadYaw"}),
-				// new Variant(new float[] {1.0f}));
-				motion.angleInterpolation(names, angles, times, true);
-				// motion.setStiffnesses(new Variant(new String[] {"HeadYaw"}),
-				// new Variant(new float[] {0.0f}));
-			}
-			if (str.compareTo("m") == 0) {
-				// motion.setStiffnesses(new Variant(new String ("Body")), new
-				// Variant(new Float (1.0f)));
-				motion.moveTo(1.2f, 1.2f, 0.0f);
-				// motion.setStiffnesses(new Variant(new String ("Body")), new
-				// Variant(new Float (0.0f)));
-			}
-		}
-		motion.setStiffnesses(new Variant(new String("Body")), new Variant(
-				new Float(0.0f)));
-		sc.close();
-	}
+        System.out.println(motion.getSummary());
+    }
 
+    /**
+     * Have Nao walk forward.
+     */
+    public void Walk() {
+        // Set move posture
+        motion.moveInit();
+
+        // Wait for ready.
+        motion.waitUntilMoveIsFinished();
+
+        // Move forward
+        motion.moveTo(0.5f, 0.0f, 0.0f);
+
+        System.out.println(motion.getSummary());
+    }
+
+    /**
+     * Have Nao turn.
+     *
+     * @param turnAmount Degrees to turn from current heading.
+     */
+    public void Turn(float turnAmount) {
+        System.out.println("Turning " + turnAmount + " degrees.");
+
+        // Get to ready position.
+        motion.moveInit();
+
+        // Wait for hardward to finish moving
+        motion.waitUntilMoveIsFinished();
+
+        // Convert the turn amount to radians.
+        float moveAmountRadians = DegreesToRadians(turnAmount);
+
+        // Send turn command.
+        motion.moveTo(0.0f, 0.0f, moveAmountRadians);
+
+        System.out.println(motion.getSummary());
+
+    }
+
+    /**
+     * Have Nao Step left or right.
+     *
+     * @param left True to move left false to move right.
+     */
+    public void Strafe(boolean left) {
+        System.out.println("Stepping to the " + (left ? "left" : "right"));
+
+        // Get ready to turn.
+        motion.moveInit();
+
+        // Wait for hardward to finish moving.
+        motion.waitUntilMoveIsFinished();
+
+        // Start stepping
+        if (left) {
+            motion.moveTo(0.0f, -0.5f, 0.0f);
+        } else {
+            motion.moveTo(0.0f, 0.5f, 0.0f);
+        }
+
+        System.out.println(motion.getSummary());
+    }
+
+    /**
+     * Stand Nao up.
+     */
+    public void Stand() {
+        motion.stopMove();
+        posture.goToPosture("Stand", 1.0f);
+        System.out.println("Standing Up.");
+    }
+
+    /**
+     * Sit Nao down.
+     */
+    public void Sit() {
+        motion.stopMove();
+        posture.goToPosture("Sit", 1.0f);
+        System.out.println("Sitting Down.");
+    }
+
+    public void Stop() {
+        motion.stopMove();
+        System.out.println("Stopping Movement.");
+    }
+
+    public void emergencyStop() {
+        motion.killAll();
+    }
+
+    public void testMethod() {
+        // This lets you use bound methods that expects ALValue from Java:
+        Variant names = new Variant(new String[]{"HeadYaw"});
+        Variant angles = new Variant(new float[]{-0.5f, 0.5f, 0.0f});
+        Variant times = new Variant(new float[]{1.0f, 2.0f, 3.0f});
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+
+        motion.setStiffnesses(new Variant(new String("Body")), new Variant(
+                new Float(1.0f)));
+        posture.goToPosture("Stand", 0.5f);
+
+        while (str.compareTo("x") != 0) {
+            str = sc.next();
+            if (str.compareTo("h") == 0) {
+                // motion.setStiffnesses(new Variant(new String[] {"HeadYaw"}),
+                // new Variant(new float[] {1.0f}));
+                motion.angleInterpolation(names, angles, times, true);
+                // motion.setStiffnesses(new Variant(new String[] {"HeadYaw"}),
+                // new Variant(new float[] {0.0f}));
+            }
+            if (str.compareTo("m") == 0) {
+                // motion.setStiffnesses(new Variant(new String ("Body")), new
+                // Variant(new Float (1.0f)));
+                motion.moveTo(1.2f, 1.2f, 0.0f);
+                // motion.setStiffnesses(new Variant(new String ("Body")), new
+                // Variant(new Float (0.0f)));
+            }
+        }
+        motion.setStiffnesses(new Variant(new String("Body")), new Variant(
+                new Float(0.0f)));
+        sc.close();
+    }
+
+    private static float DegreesToRadians(float deg) {
+        return (deg * (float) (Math.PI / 180.0));
+    }
 }
